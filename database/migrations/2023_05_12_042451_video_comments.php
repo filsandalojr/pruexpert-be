6d1c1cbbfb073d7b9fee7c4f6775aaa90293c6eb
Fil Sandalo Jr <filadelfo.sandalo.jr@prudential.com.sg>
2023-05-15 11:38:20 +0800

    digital trigger

---
+++ database/migrations/2023_05_12_042451_video_comments.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video_comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('course_id');
+            $table->string('module_id');
+            $table->text('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video_comments');
+    }
+};
